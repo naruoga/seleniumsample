5322a6ca91977ccc5fbcde67b32e373fb5894ae1
Naruhiko Ogasawara <naruoga@gmail.com>
2017-08-21 23:25:20 +0900

    ExampleTest: Selenideで書き直し
    - ついでにスクショのテストを追加

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -3,14 +3,14 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
 
+import static com.codeborne.selenide.Selenide.*;
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ExampleTest {
@@ -32,31 +32,40 @@ public class ExampleTest {
     @Before
     public void setup() {
         System.setProperty("webdriver.chrome.driver", chromeDriverPath());
-        driver = new ChromeDriver();
+        System.setProperty("selenide.browser", "Chrome");
     }
 
     @After
     public void teardown() {
-        driver.quit();
+        close();
     }
 
     @Test
     public void Googleにアクセスしタイトルを調べる() {
-        driver.get(googleUrl);
-        assertThat(driver.getTitle(), containsString("Google"));
+        open(googleUrl);
+        assertThat(title(), containsString("Google"));
     }
 
     @Test
     public void Googleで検索を行う() throws InterruptedException {
-        driver.get(googleUrl);
-        driver.findElement(By.name("q")).sendKeys("Selenium" + Keys.RETURN);
-        assertThat(driver.getTitle(), containsString("Selenium - Google"));
+        open(googleUrl);
+        $("[name='q'").setValue("Selenium").sendKeys(Keys.RETURN);
+        assertThat(title(), containsString("Selenium - Google"));
     }
 
     @Ignore
     @Test
     public void 常に失敗する() {
-        driver.get(googleUrl);
-        assertThat(driver.getTitle(), containsString("Gaagle"));
+        open(googleUrl);
+        assertThat(title(), containsString("Gaagle"));
+    }
+
+    @Test
+    public void スクリーンショットを取る() {
+        open(googleUrl);
+
+        screenshot("screenshot");
+        File png = new File("build/reports/tests/screenshot.png");
+        assertThat(png.exists(), equalTo(true));
     }
 }
